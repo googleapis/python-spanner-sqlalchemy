18bfb99d9101e935ae4fe0cdfd7569a2086cb274
HemangChothani <hemang.chothani@qlogic.io>
2021-03-01 20:15:01 +0530

    fix: datetime compliance test

--- test/test_suite.py
+++ test/test_suite.py
@@ -14,20 +14,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from sqlalchemy.testing.suite.test_dialect import *  # noqa: F401, F403
 from sqlalchemy.testing import config
 from sqlalchemy.testing import eq_
 from sqlalchemy.testing import provide_metadata
 from sqlalchemy.testing.schema import Column
 from sqlalchemy.testing.schema import Table
 from sqlalchemy import literal_column
-from sqlalchemy import select
+from sqlalchemy import select, case, bindparam
 from sqlalchemy import String
+from sqlalchemy.testing import requires
+from sqlalchemy.types import Integer
+from google.api_core.datetime_helpers import DatetimeWithNanoseconds
 
 from sqlalchemy.testing.suite.test_update_delete import *  # noqa: F401, F403
-from sqlalchemy.testing.suite.test_dialect import *  # noqa: F401, F403
 
 from sqlalchemy.testing.suite.test_dialect import (  # noqa: F401, F403
     EscapingTest as _EscapingTest,
+)
+
+from sqlalchemy.testing.suite.test_types import (  # noqa: F401, F403
+    DateTest as _DateTest,
+)
+
+from sqlalchemy.testing.suite.test_types import (  # noqa: F401, F403
+    DateTimeMicrosecondsTest as _DateTimeMicrosecondsTest,
+)
+
+from sqlalchemy.testing.suite.test_types import (  # noqa: F401, F403
+    DateTimeTest as _DateTimeTest,
 )
 
 
@@ -68,3 +83,112 @@ class EscapingTest(_EscapingTest):
                 ),
                 "some %% other value",
             )
+
+
+class DateTest(_DateTest):
+    def test_round_trip(self):
+        date_table = self.tables.date_table
+
+        config.db.execute(date_table.insert(), {"id": 1, "date_data": self.data})
+
+        row = config.db.execute(select([date_table.c.date_data])).first()
+
+        compare = self.compare or self.data
+        eq_(row, (compare,))
+        assert isinstance(row[0], type(compare))
+
+    def test_null(self):
+        date_table = self.tables.date_table
+
+        config.db.execute(date_table.insert(), {"id": 1, "date_data": None})
+
+        row = config.db.execute(select([date_table.c.date_data])).first()
+        eq_(row, (None,))
+
+    @requires.standalone_null_binds_whereclause
+    def test_null_bound_comparison(self):
+        # this test is based on an Oracle issue observed in #4886.
+        # passing NULL for an expression that needs to be interpreted as
+        # a certain type, does the DBAPI have the info it needs to do this.
+        date_table = self.tables.date_table
+        with config.db.connect() as conn:
+            result = conn.execute(
+                date_table.insert(), {"id": 1, "date_data": self.data}
+            )
+            id_ = result.inserted_primary_key[0]
+            stmt = select([date_table.c.id]).where(
+                case(
+                    [
+                        (
+                            bindparam("foo", type_=self.datatype) is not None,
+                            bindparam("foo", type_=self.datatype),
+                        )
+                    ],
+                    else_=date_table.c.date_data,
+                )
+                == date_table.c.date_data
+            )
+
+            row = conn.execute(stmt, {"foo": None}).first()
+            eq_(row[0], id_)
+
+
+class DateTimeMicrosecondsTest(_DateTimeMicrosecondsTest):
+    @classmethod
+    def define_tables(cls, metadata):
+        Table(
+            "datetime_table",
+            metadata,
+            Column("id", Integer, primary_key=True, test_needs_autoincrement=True),
+            Column("date_data", cls.datatype),
+        )
+
+    def test_null(self):
+        date_table = self.tables.datetime_table
+
+        config.db.execute(date_table.insert(), {"id": 1, "date_data": None})
+
+        row = config.db.execute(select([date_table.c.date_data])).first()
+        eq_(row, (None,))
+
+    def test_round_trip(self):
+        date_table = self.tables.datetime_table
+
+        config.db.execute(date_table.insert(), {"id": 1, "date_data": self.data})
+
+        row = config.db.execute(select([date_table.c.date_data])).first()
+        compare = self.compare or self.data
+        compare = compare.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        eq_(row[0].rfc3339(), compare)
+        assert isinstance(row[0], DatetimeWithNanoseconds)
+
+    @requires.standalone_null_binds_whereclause
+    def test_null_bound_comparison(self):
+        # this test is based on an Oracle issue observed in #4886.
+        # passing NULL for an expression that needs to be interpreted as
+        # a certain type, does the DBAPI have the info it needs to do this.
+        date_table = self.tables.datetime_table
+        with config.db.connect() as conn:
+            result = conn.execute(
+                date_table.insert(), {"id": 1, "date_data": self.data}
+            )
+            id_ = result.inserted_primary_key[0]
+            stmt = select([date_table.c.id]).where(
+                case(
+                    [
+                        (
+                            bindparam("foo", type_=self.datatype) is not None,
+                            bindparam("foo", type_=self.datatype),
+                        )
+                    ],
+                    else_=date_table.c.date_data,
+                )
+                == date_table.c.date_data
+            )
+
+            row = conn.execute(stmt, {"foo": None}).first()
+            eq_(row[0], id_)
+
+
+class DateTimeTest(_DateTimeTest, DateTimeMicrosecondsTest):
+    pass
