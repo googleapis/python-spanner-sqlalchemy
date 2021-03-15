f4b82cfb441304c4999154fc851d4bfb8d420ad2
IlyaFaer <ilya.faer@mail.ru>
2021-03-15 12:38:38 +0300

    feat: implement UNIQUE constraints creation mechanism

--- test/test_suite.py
+++ test/test_suite.py
@@ -14,21 +14,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import operator
 import pytest
 
+import sqlalchemy
+from sqlalchemy import inspect
+from sqlalchemy import testing
+from sqlalchemy import MetaData
+from sqlalchemy.schema import DDL
 from sqlalchemy.testing import config
 from sqlalchemy.testing import eq_
 from sqlalchemy.testing import provide_metadata
+from sqlalchemy.testing.provision import temp_table_keyword_args
 from sqlalchemy.testing.schema import Column
 from sqlalchemy.testing.schema import Table
 from sqlalchemy import literal_column
+from sqlalchemy import event
 from sqlalchemy import exists
 from sqlalchemy import select
 from sqlalchemy import Boolean
 from sqlalchemy import String
 
-from sqlalchemy.testing.suite.test_ddl import *  # noqa: F401, F403
+from google.cloud import spanner_dbapi
+
+from sqlalchemy.testing.suite.test_reflection import *  # noqa: F401, F403
+
 from sqlalchemy.testing.suite.test_cte import *  # noqa: F401, F403
+from sqlalchemy.testing.suite.test_ddl import *  # noqa: F401, F403
 from sqlalchemy.testing.suite.test_dialect import *  # noqa: F401, F403
 from sqlalchemy.testing.suite.test_update_delete import *  # noqa: F401, F403
 
@@ -37,8 +49,10 @@ from sqlalchemy.testing.suite.test_ddl import TableDDLTest as _TableDDLTest
 from sqlalchemy.testing.suite.test_ddl import (
     LongNameBlowoutTest as _LongNameBlowoutTest,
 )
-
 from sqlalchemy.testing.suite.test_dialect import EscapingTest as _EscapingTest
+from sqlalchemy.testing.suite.test_reflection import (
+    ComponentReflectionTest as _ComponentReflectionTest,
+)
 from sqlalchemy.testing.suite.test_select import ExistsTest as _ExistsTest
 from sqlalchemy.testing.suite.test_types import BooleanTest as _BooleanTest
 
@@ -208,3 +222,136 @@ class TableDDLTest(_TableDDLTest):
 @pytest.mark.skip("Max identifier length in Spanner is 128")
 class LongNameBlowoutTest(_LongNameBlowoutTest):
     pass
+
+
+class ComponentReflectionTest(_ComponentReflectionTest):
+    @classmethod
+    def define_temp_tables(cls, metadata):
+        """
+        SPANNER OVERRIDE:
+
+        In Cloud Spanner unique indexes are used instead of directly
+        creating unique constraints. Overriding the test to replace
+        constraints with indexes in testing data.
+        """
+        kw = temp_table_keyword_args(config, config.db)
+        user_tmp = Table(
+            "user_tmp",
+            metadata,
+            Column("id", sqlalchemy.INT, primary_key=True),
+            Column("name", sqlalchemy.VARCHAR(50)),
+            Column("foo", sqlalchemy.INT),
+            sqlalchemy.Index("user_tmp_uq", "name", unique=True),
+            sqlalchemy.Index("user_tmp_ix", "foo"),
+            **kw
+        )
+        if (
+            testing.requires.view_reflection.enabled
+            and testing.requires.temporary_views.enabled
+        ):
+            event.listen(
+                user_tmp,
+                "after_create",
+                DDL("create temporary view user_tmp_v as " "select * from user_tmp"),
+            )
+            event.listen(user_tmp, "before_drop", DDL("drop view user_tmp_v"))
+
+    @testing.provide_metadata
+    def _test_get_unique_constraints(self, schema=None):
+        """
+        SPANNER OVERRIDE:
+
+        In Cloud Spanner unique indexes are used instead of directly
+        creating unique constraints. Overriding the test to replace
+        constraints with indexes in testing data.
+        """
+        # SQLite dialect needs to parse the names of the constraints
+        # separately from what it gets from PRAGMA index_list(), and
+        # then matches them up.  so same set of column_names in two
+        # constraints will confuse it.    Perhaps we should no longer
+        # bother with index_list() here since we have the whole
+        # CREATE TABLE?
+        uniques = sorted(
+            [
+                {"name": "unique_a", "column_names": ["a"]},
+                {"name": "unique_a_b_c", "column_names": ["a", "b", "c"]},
+                {"name": "unique_c_a_b", "column_names": ["c", "a", "b"]},
+                {"name": "unique_asc_key", "column_names": ["asc", "key"]},
+                {"name": "i.have.dots", "column_names": ["b"]},
+                {"name": "i have spaces", "column_names": ["c"]},
+            ],
+            key=operator.itemgetter("name"),
+        )
+        orig_meta = self.metadata
+        table = Table(
+            "testtbl",
+            orig_meta,
+            Column("a", sqlalchemy.String(20)),
+            Column("b", sqlalchemy.String(30)),
+            Column("c", sqlalchemy.Integer),
+            # reserved identifiers
+            Column("asc", sqlalchemy.String(30)),
+            Column("key", sqlalchemy.String(30)),
+            schema=schema,
+        )
+        for uc in uniques:
+            table.append_constraint(
+                sqlalchemy.Index(uc["name"], *uc["column_names"], unique=True)
+            )
+        orig_meta.create_all()
+
+        inspector = inspect(orig_meta.bind)
+        reflected = sorted(
+            inspector.get_unique_constraints("testtbl", schema=schema),
+            key=operator.itemgetter("name"),
+        )
+
+        names_that_duplicate_index = set()
+
+        for orig, refl in zip(uniques, reflected):
+            # Different dialects handle duplicate index and constraints
+            # differently, so ignore this flag
+            dupe = refl.pop("duplicates_index", None)
+            if dupe:
+                names_that_duplicate_index.add(dupe)
+            eq_(orig, refl)
+
+        reflected_metadata = MetaData()
+        reflected = Table(
+            "testtbl", reflected_metadata, autoload_with=orig_meta.bind, schema=schema,
+        )
+
+        # test "deduplicates for index" logic.   MySQL and Oracle
+        # "unique constraints" are actually unique indexes (with possible
+        # exception of a unique that is a dupe of another one in the case
+        # of Oracle).  make sure # they aren't duplicated.
+        idx_names = set([idx.name for idx in reflected.indexes])
+        uq_names = set(
+            [
+                uq.name
+                for uq in reflected.constraints
+                if isinstance(uq, sqlalchemy.UniqueConstraint)
+            ]
+        ).difference(["unique_c_a_b"])
+
+        assert not idx_names.intersection(uq_names)
+        if names_that_duplicate_index:
+            eq_(names_that_duplicate_index, idx_names)
+            eq_(uq_names, set())
+
+    @testing.provide_metadata
+    def test_unique_constraint_raises(self):
+        """
+        Checking that unique constraint creation
+        fails due to a ProgrammingError.
+        """
+        Table(
+            "user_tmp_failure",
+            self.metadata,
+            Column("id", sqlalchemy.INT, primary_key=True),
+            Column("name", sqlalchemy.VARCHAR(50)),
+            sqlalchemy.UniqueConstraint("name", name="user_tmp_uq"),
+        )
+
+        with pytest.raises(spanner_dbapi.exceptions.ProgrammingError):
+            self.metadata.create_all()
