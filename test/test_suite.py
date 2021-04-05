ae5e8394c85fde28fc7f1980f4497eba87615b01
IlyaFaer <ilya.faer@mail.ru>
2021-04-05 12:04:49 +0300

    skip empty inserts

--- test/test_suite.py
+++ test/test_suite.py
@@ -58,6 +58,9 @@ from sqlalchemy.testing.suite.test_ddl import (
     LongNameBlowoutTest as _LongNameBlowoutTest,
 )
 from sqlalchemy.testing.suite.test_dialect import EscapingTest as _EscapingTest
+from sqlalchemy.testing.suite.test_insert import (
+    InsertBehaviorTest as _InsertBehaviorTest,
+)
 from sqlalchemy.testing.suite.test_reflection import (
     ComponentReflectionTest as _ComponentReflectionTest,
 )
@@ -689,4 +692,10 @@ class ComponentReflectionTest(_ComponentReflectionTest):
 
     @pytest.mark.skip("Spanner doesn't support temporary tables")
     def test_get_temp_table_unique_constraints(self):
+        pass
+
+
+class InsertBehaviorTest(_InsertBehaviorTest):
+    @pytest.mark.skip("Spanner doesn't support empty inserts")
+    def test_empty_insert(self):
         pass
