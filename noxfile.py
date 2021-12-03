ec03f08a0a19e968afd0531fa013bfa868675a4d
larkee <larkee@users.noreply.github.com>
2021-12-03 20:49:42 +1100

    chore: remove coverage session

--- noxfile.py
+++ noxfile.py
@@ -244,16 +244,3 @@ def snippets(session):
         os.path.join("samples", "snippets_test.py"),
         *session.posargs,
     )
-
-
-@nox.session(python=DEFAULT_PYTHON_VERSION)
-def cover(session):
-    """Run the final coverage report.
-
-    This outputs the coverage report aggregating coverage from the unit
-    test runs (not system test runs), and then erases coverage data.
-    """
-    session.install("coverage", "pytest-cov")
-    session.run("coverage", "report", "--show-missing", "--fail-under=99")
-
-    session.run("coverage", "erase")
