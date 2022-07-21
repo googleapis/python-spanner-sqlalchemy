1217cef0fdfba18b992cb4494b73a71afceaf4d5
Ilya Gurov <gurovilya9@gmail.com>
2022-07-21 01:01:47 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ with open(VERSION_FILENAME) as f:
     exec(f.read(), PACKAGE_INFO)
 version = PACKAGE_INFO["__version__"]
 
+package_root = os.path.abspath(os.path.dirname(__file__))
 readme_filename = os.path.join(package_root, "README.rst")
 with io.open(readme_filename, encoding="utf-8") as readme_file:
     readme = readme_file.read()
