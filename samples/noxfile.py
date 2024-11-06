8664aca28cb8f6fffdd5288029ee552263d18722
Knut Olav Løite <koloite@gmail.com>
2024-11-06 20:26:01 +0100

    docs: add samples for Spanner-specific features

---
+++ samples/noxfile.py
@@ -0,0 +1,36 @@
+# Copyright 2024 Google LLC All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import nox
+
+
+@nox.session()
+def hello_world(session):
+    _sample(session)
+
+
+@nox.session()
+def create_tables(session):
+    _sample(session)
+
+
+def _sample(session):
+    session.install("testcontainers")
+    session.install("sqlalchemy")
+    session.install("setuptools")
+    session.install(
+        "git+https://github.com/googleapis/python-spanner.git#egg=google-cloud-spanner"
+    )
+    session.install("../.")
+    session.run("python", session.name + "_sample.py")
