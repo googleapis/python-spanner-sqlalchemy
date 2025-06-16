69ea1a2615565ac83fd4b4b4a681d1739bfb9a96
Walt Askew <waltaskew@google.com>
2025-06-16 19:10:46 +0000

    feat: support commit timestamp option
    Add support for columns with commit timestamps:
    https://cloud.google.com/spanner/docs/commit-timestamp
    
    Fixes: #695

--- samples/model.py
+++ samples/model.py
@@ -33,6 +33,8 @@ from sqlalchemy import (
     TextClause,
     Index,
     PickleType,
+    text,
+    event,
 )
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from google.cloud.sqlalchemy_spanner.sqlalchemy_spanner import SpannerPickleType
@@ -75,6 +77,21 @@ class Singer(Base):
     concerts: Mapped[List["Concert"]] = relationship(
         back_populates="singer", cascade="all, delete-orphan"
     )
+    # Create a commit timestamp column and set a client-side default of
+    # PENDING_COMMIT_TIMESTAMP() An event handler below is responsible for
+    # setting PENDING_COMMIT_TIMESTAMP() on updates. If using SQLAlchemy
+    # core rather than the ORM, callers will need to supply their own
+    # PENDING_COMMIT_TIMESTAMP() values in their inserts & updates.
+    last_update_time: Mapped[datetime.datetime] = mapped_column(
+        spanner_allow_commit_timestamp=True,
+        default=text("PENDING_COMMIT_TIMESTAMP()"),
+    )
+
+
+@event.listens_for(TimestampUser, "before_update")
+def singer_before_update(mapper, connection, target):
+    """Updates the commit timestamp when the row is updated."""
+    target.updated_at = text("PENDING_COMMIT_TIMESTAMP()")
 
 
 class Album(Base):
